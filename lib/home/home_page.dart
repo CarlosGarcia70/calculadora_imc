22efde80b343f118d96ba8a37d5b371a1c9798d6
Carlos Adriano Garcia <carlosadrianogarci@gmail.com>
2020-12-05 11:45:55 -0300

    Initial commit

---
+++ lib/home/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:calculadora_imc/home/home_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomePage extends StatelessWidget {
+
+  final controller = Get.put<HomeController>(HomeController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'home/home_bindings.dart';
+import 'home/home_page.dart';
+
+void main() {
+  runApp(GetMaterialApp(
+      title: 'Calculadora de IMC com GetX',
+      initialRoute: '/',
+      theme: ThemeData(
+          primarySwatch: Colors.blue
+      ),
+      getPages: [
+        GetPage(name: '/', page: () => HomePage(), binding: HomeBinding())
+      ]
+  ));
+}
